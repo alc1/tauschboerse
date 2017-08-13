fad31ac91cad76503dec57b782148dc4528d4598
Christian Albiez <calbiez@hsr.ch>
2017-08-13 21:26:01 +0200

    Cleaner separation of store and reducers (redux)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
-import reducers from './client/reducers/store';
-
+import store from './client/store/store'
 import App from './client/App';
-
-const store = createStore(
-    reducers,
-    applyMiddleware(thunk)
-);
 
 const provider = (
     <Provider store={store}>
