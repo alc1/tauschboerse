79a937034ebac062b75000bcfec580194088178c
Christian Albiez <calbiez@hsr.ch>
2017-08-18 11:16:21 +0200

    User details added to article in server API

--- src/server/controller/articlesController.js
+++ src/server/controller/articlesController.js
@@ -1,19 +1,36 @@
 'use strict';
 
 const articlesStore = require('../services/articlesStorage');
+const usersStore = require('../services/usersStorage');
 
 function getArticlesByUser(req, res) {
     const userId = req.params.userId;
     articlesStore.getArticlesByUser(userId, (err, articles) => {
-        res.json({ articles : articles || [] });
+        usersStore.getUser(userId, (err, user) => {
+            articles.forEach((article) => {
+                addUserDetailsToArticle(article, user);
+            });
+            res.json({ articles : articles || [] });
+        });
     });
 }
 
 function getArticle(req, res) {
     const articleId = req.params.articleId;
     articlesStore.getArticle(articleId, (err, article) => {
-        res.json({ article : article || {} });
+        usersStore.getUser(article.userId, (err, user) => {
+            addUserDetailsToArticle(article, user);
+            res.json({ article : article || {} });
+        });
     });
+}
+
+function addUserDetailsToArticle(theArticle, theUser) {
+    delete theArticle.userId;
+    theArticle.user = {
+        _id: theUser._id,
+        name : theUser.name
+    };
 }
 
 module.exports = {
