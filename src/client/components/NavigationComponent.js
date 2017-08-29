6cbc2fab81f7a8ef80153a229043613137c487c3
Christian Albiez <calbiez@hsr.ch>
2017-08-29 20:45:03 +0200

    Trial with styled-components

--- src/client/components/NavigationComponent.js
+++ src/client/components/NavigationComponent.js
@@ -10,6 +10,8 @@ import CompareArrows from 'material-ui/svg-icons/action/compare-arrows';
 import AccountCircle from 'material-ui/svg-icons/action/account-circle';
 import PersonAdd from 'material-ui/svg-icons/social/person-add';
 
+import styled from 'styled-components';
+
 class NavigationComponent extends React.Component {
     render() {
         let userTransactionsLink = '/';
@@ -21,17 +23,31 @@ class NavigationComponent extends React.Component {
             userDetailsLink = `/user/${this.props.user._id}/details`;
         }
 
+        const NavigationDiv = styled.div`
+            display: flex;
+            flex-direction: row;
+            justify-content: flex-start;
+            align-items: flex-start;
+            margin: 0.6em;
+        
+            @media (max-width: 750px) {
+                flex-direction: column;
+            }
+        `;
+
         return (
             <div>
                 {this.props.isMenuOpen &&
-                <Toolbar>
-                    <ToolbarGroup>
-                        <Link to="/"><FlatButton label="Home" icon={<Home/>} primary/></Link>
-                        <Link to="/marketplace"><FlatButton label="Marktplatz" icon={<Business/>} primary/></Link>
-                        {this.props.isLoggedIn && <Link to={userTransactionsLink}><FlatButton label="Tauschgeschäfte" icon={<CompareArrows/>} primary/></Link>}
-                        {this.props.isLoggedIn && <Link to={userArticlesLink}><FlatButton label="Meine Artikel" icon={<List/>} primary/></Link>}
-                        {this.props.isLoggedIn && <Link to={userDetailsLink}><FlatButton label="Profil" icon={<AccountCircle/>} primary/></Link>}
-                        {!this.props.isLoggedIn && <Link to="/registration"><FlatButton label="Registrieren" icon={<PersonAdd/>} primary/></Link>}
+                <Toolbar style={{'height': 'auto'}}>
+                    <ToolbarGroup firstChild>
+                        <NavigationDiv>
+                            <Link to="/"><FlatButton label="Home" icon={<Home/>} primary/></Link>
+                            <Link to="/marketplace"><FlatButton label="Marktplatz" icon={<Business/>} primary/></Link>
+                            {this.props.isLoggedIn && <Link to={userTransactionsLink}><FlatButton label="Tauschgeschäfte" icon={<CompareArrows/>} primary/></Link>}
+                            {this.props.isLoggedIn && <Link to={userArticlesLink}><FlatButton label="Artikel" icon={<List/>} primary/></Link>}
+                            {this.props.isLoggedIn && <Link to={userDetailsLink}><FlatButton label="Profil" icon={<AccountCircle/>} primary/></Link>}
+                            {!this.props.isLoggedIn && <Link to="/registration"><FlatButton label="Registrieren" icon={<PersonAdd/>} primary/></Link>}
+                        </NavigationDiv>
                     </ToolbarGroup>
                 </Toolbar>}
             </div>
