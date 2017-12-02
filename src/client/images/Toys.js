78a12b17ad2fe27a8fbc9063f1054075f2fe6844
Christian Albiez <calbiez@hsr.ch>
2017-12-02 21:28:14 +0100

    Start page is now responsive and has animation

--- src/client/images/Toys.js
+++ src/client/images/Toys.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Toys(props) {
     return (
-        <svg fill={props.color} height={props.height} viewBox="0 0 24 24" width={props.width} xmlns="http://www.w3.org/2000/svg">
+        <svg {...props} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path d="M12 12c0-3 2.5-5.5 5.5-5.5S23 9 23 12H12zm0 0c0 3-2.5 5.5-5.5 5.5S1 15 1 12h11zm0 0c-3 0-5.5-2.5-5.5-5.5S9 1 12 1v11zm0 0c3 0 5.5 2.5 5.5 5.5S15 23 12 23V12z"/>
             <path d="M0 0h24v24H0z" fill="none"/>
         </svg>
--- src/client/pages/HomePage.js
+++ src/client/pages/HomePage.js
@@ -10,10 +10,9 @@ import MarketplaceIcon from 'material-ui/svg-icons/communication/business';
 import LoginIcon from 'material-ui/svg-icons/action/lock-open';
 import NewAccountIcon from 'material-ui/svg-icons/social/person-add';
 import ExitIcon from 'material-ui/svg-icons/action/exit-to-app';
-import { cyan300, cyan200, cyan100 } from 'material-ui/styles/colors';
 
 import ApplicationBar from '../components/ApplicationBar';
-import SwapHorizontal from '../images/SwapHorizontal';
+import Swap from '../images/Swap';
 import Camera from '../images/Camera';
 import Flower from '../images/Flower';
 import Bike from '../images/Bike';
@@ -42,26 +41,29 @@ class HomePage extends React.Component {
 
     render() {
         const { user } = this.props;
-        const { primary1Color } = this.props.muiTheme.palette;
         const { fontFamily } = this.props.muiTheme;
         return (
             <div>
                 <ApplicationBar/>
                 <div className="homepage__container">
-                    <h1 className="homepage__title" style={{ fontFamily: fontFamily }}>Willkommen an der Tauschbörse!</h1>
+                    <h1 className="homepage__title" style={{ fontFamily: fontFamily }}>Willkommen bei der Tauschbörse!</h1>
                     <div className="homepage__image-container">
-                        <Camera width={50} height={50} color={cyan100}/>
-                        <Flower width={100} height={100} color={cyan200}/>
-                        <Bike width={150} height={150} color={cyan300}/>
+                        <div className="homepage__images-group">
+                            <Camera className="homepage__image1"/>
+                            <Flower className="homepage__image2"/>
+                            <Bike className="homepage__image3"/>
+                        </div>
                         <Paper zDepth={3} circle style={{ margin: '10px' }}>
-                            <SwapHorizontal width={250} height={250} color={primary1Color}/>
+                            <Swap className="homepage__swap-image"/>
                         </Paper>
-                        <Toys width={150} height={150} color={cyan300}/>
-                        <Watch width={100} height={100} color={cyan200}/>
-                        <Tablet width={50} height={50} color={cyan100}/>
+                        <div className="homepage__images-group">
+                            <Toys className="homepage__image3"/>
+                            <Watch className="homepage__image2"/>
+                            <Tablet className="homepage__image1"/>
+                        </div>
                     </div>
                     {user && <span className="homepage__text" style={{ fontFamily: fontFamily }}>Hallo {user.name}</span>}
-                    <RaisedButton style={buttonStyle} label="Zum Marktplatz" icon={<MarketplaceIcon/>} onClick={this.goTo.bind(this, '/marketplace')} primary/>
+                    <RaisedButton style={buttonStyle} label="Marktplatz durchstöbern" icon={<MarketplaceIcon/>} onClick={this.goTo.bind(this, '/marketplace')} primary/>
                     {!user && <RaisedButton style={buttonStyle} label="Anmelden" icon={<LoginIcon/>} onClick={this.goTo.bind(this, '/login')} primary/>}
                     {!user && <FlatButton style={buttonStyle} label="Neues Konto erstellen" icon={<NewAccountIcon/>} onClick={this.goTo.bind(this, '/registration')} secondary/>}
                     {user && <RaisedButton style={buttonStyle} label="Abmelden" icon={<ExitIcon/>} onClick={this.props.logout} secondary/>}
