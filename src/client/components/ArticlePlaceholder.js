4a4de49704e137baf4b50b4a654bba18052f22fa
Christian Albiez <calbiez@hsr.ch>
2017-12-06 11:42:52 +0100

    SVG images refactored to reusable image components

--- src/client/components/ArticlePlaceholder.js
+++ src/client/components/ArticlePlaceholder.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 
 import muiThemeable from 'material-ui/styles/muiThemeable';
 
+import Loading from '../images/Loading';
+import Info from '../images/Info';
+
 import './ArticlePlaceholder.css';
 
 class ArticlePlaceholder extends React.Component {
@@ -15,23 +18,18 @@ class ArticlePlaceholder extends React.Component {
 
     render() {
         const { width, height, loading } = this.props;
+        const { fontFamily } = this.props.muiTheme;
         return (
             <div>
                 {loading ? (
                     <div className="article-placeholder__container">
-                        <svg fill="#E1E1E1" height={height} viewBox="0 0 24 24" width={width} xmlns="http://www.w3.org/2000/svg">
-                            <path d="M12 4V1L8 5l4 4V6c3.31 0 6 2.69 6 6 0 1.01-.25 1.97-.7 2.8l1.46 1.46C19.54 15.03 20 13.57 20 12c0-4.42-3.58-8-8-8zm0 14c-3.31 0-6-2.69-6-6 0-1.01.25-1.97.7-2.8L5.24 7.74C4.46 8.97 4 10.43 4 12c0 4.42 3.58 8 8 8v3l4-4-4-4v3z"/>
-                            <path d="M0 0h24v24H0z" fill="none"/>
-                        </svg>
-                        <span className="article-placeholder__text" style={{ fontFamily: this.props.muiTheme.fontFamily }}>... Artikel werden geladen ...</span>
+                        <Loading fill="#E1E1E1" width={width} height={height}/>
+                        <span className="article-placeholder__text" style={{ fontFamily: fontFamily }}>... Artikel werden geladen ...</span>
                     </div>
                 ) : (
                     <div className="article-placeholder__container">
-                        <svg fill="#E1E1E1" height={height} viewBox="0 0 24 24" width={width} xmlns="http://www.w3.org/2000/svg">
-                            <path d="M0 0h24v24H0z" fill="none"/>
-                            <path d="M11 17h2v-6h-2v6zm1-15C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM11 9h2V7h-2v2z"/>
-                        </svg>
-                        <span className="article-placeholder__text" style={{ fontFamily: this.props.muiTheme.fontFamily }}>Keine Artikel gefunden</span>
+                        <Info fill="#E1E1E1" width={width} height={height}/>
+                        <span className="article-placeholder__text" style={{ fontFamily: fontFamily }}>Keine Artikel gefunden</span>
                     </div>
                 )}
             </div>
--- src/client/components/PhotoPlaceholder.js
+++ src/client/components/PhotoPlaceholder.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Photo from '../images/Photo';
+
 export default class PhotoPlaceholder extends React.Component {
 
     static propTypes = {
@@ -11,10 +13,7 @@ export default class PhotoPlaceholder extends React.Component {
     render() {
         const { width, height } = this.props;
         return (
-            <svg fill="#E1E1E1" height={height} viewBox="0 0 24 24" width={width} xmlns="http://www.w3.org/2000/svg">
-                <path d="M4 4h7V2H4c-1.1 0-2 .9-2 2v7h2V4zm6 9l-4 5h12l-3-4-2.03 2.71L10 13zm7-4.5c0-.83-.67-1.5-1.5-1.5S14 7.67 14 8.5s.67 1.5 1.5 1.5S17 9.33 17 8.5zM20 2h-7v2h7v7h2V4c0-1.1-.9-2-2-2zm0 18h-7v2h7c1.1 0 2-.9 2-2v-7h-2v7zM4 13H2v7c0 1.1.9 2 2 2h7v-2H4v-7z"/>
-                <path d="M0 0h24v24H0z" fill="none"/>
-            </svg>
+            <Photo fill="#E1E1E1" width={width} height={height}/>
         );
     }
 }
