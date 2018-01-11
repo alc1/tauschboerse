217808c058af81736c1fbdc27982d9e9bb5530fe
Christian Albiez <calbiez@hsr.ch>
2018-01-11 15:37:56 +0100

    Bug fixed in common error handler

--- src/client/store/actions/common.js
+++ src/client/store/actions/common.js
@@ -1,23 +1,31 @@
 import { globalMessageReceived, ERROR_MESSAGE, GO_TO_LOGIN } from './application';
 
 export const handleError = (error, dispatch) => {
-    if (error.response.status === 401 || error.response.status === 403) {
-        dispatch(globalMessageReceived({
-            messageText: getErrorMessage(error),
-            messageType: ERROR_MESSAGE,
-            actionText: 'Login',
-            actionType: GO_TO_LOGIN
-        }));
+    if (error.response) {
+        if (error.response.status === 401 || error.response.status === 403) {
+            dispatch(globalMessageReceived({
+                messageText: getErrorMessage(error),
+                messageType: ERROR_MESSAGE,
+                actionText: 'Login',
+                actionType: GO_TO_LOGIN
+            }));
+        }
+        else if (error.response.status === 404 || error.response.status === 413) {
+            dispatch(globalMessageReceived({
+                messageText: getErrorMessage(error),
+                messageType: ERROR_MESSAGE
+            }));
+        }
+        else if (error.response.status === 500) {
+            dispatch(globalMessageReceived({
+                messageText: getErrorMessage(error),
+                messageType: ERROR_MESSAGE
+            }));
+        }
     }
-    else if (error.response.status === 404 || error.response.status === 413) {
+    else if (error.message) {
         dispatch(globalMessageReceived({
-            messageText: getErrorMessage(error),
-            messageType: ERROR_MESSAGE
-        }));
-    }
-    else if (error.response.status === 500) {
-        dispatch(globalMessageReceived({
-            messageText: getErrorMessage(error),
+            messageText: error.message,
             messageType: ERROR_MESSAGE
         }));
     }
