a7b83fa05e8eb5787e5521e245beced05573c9c2
Stephen Atchison <stephen.atchison@gmail.com>
2018-01-18 23:43:05 +0100

    ArticlesDisplayInfo extended (a little bit)

--- src/client/model/ArticlesDisplayInfo.test.js
+++ src/client/model/ArticlesDisplayInfo.test.js
@@ -1,5 +1,5 @@
 import ArticlesDisplayInfo from './ArticlesDisplayInfo';
-import { ARTICLES_FOUND } from '../store/actions/marketplace';
+import { createArticle } from '../testutils/articles';
 
 describe('ArticlesDisplayInfo', () => {
 
@@ -11,7 +11,17 @@ describe('ArticlesDisplayInfo', () => {
             expect(JSON.stringify(clonedArticlesDisplayInfo)).toEqual(JSON.stringify(articlesDisplayInfo));
         });
         test('a modified instance should return a copy of the instance', () => {
-
+            let allArticles = [];
+            for(let i = 0; i < 5; i++) {
+                let article = createArticle(i+1, `Article ${i+1}`, `This is article ${i+1}`);
+                allArticles.push(article);
+            }
+            const articles = allArticles.slice();
+            const chosenArticles = [allArticles[0], allArticles[1], allArticles[2]];
+            const articlesDisplayInfo = new ArticlesDisplayInfo().setArticles(articles).setChosenArticles(chosenArticles);
+            const clonedArticlesDisplayInfo = new ArticlesDisplayInfo(articlesDisplayInfo);
+            expect(clonedArticlesDisplayInfo).not.toBe(articlesDisplayInfo);
+            expect(JSON.stringify(clonedArticlesDisplayInfo)).toEqual(JSON.stringify(articlesDisplayInfo));
         });
     });
 
